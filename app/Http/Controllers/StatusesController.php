6060622427eb0738020e70aa9afb04fc1487f922
向洋 <827544120@qq.com>
2019-09-10 10:56:07 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use Illuminate\Http\Request;
 use Auth;
 
@@ -14,14 +15,22 @@ class StatusesController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
-            'content' => 'required|max:140'
+        $this->validate($request, [
+            'content' => 'required|max:140',
         ]);
 
         Auth::user()->statuses()->create([
-            'content' =>$request['content']
+            'content' => $request['content'],
         ]);
-        session()->flash('success','发布成功！');
+        session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
         return redirect()->back();
     }
 }
